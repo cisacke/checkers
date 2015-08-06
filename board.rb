a5cc5d38fec6cd3f0db2e22c49c628beba0bf060
App Academy Student <appacademy@Apps-Mac-mini-30.local>
2015-08-06 10:55:51 -0400

    Add #populate_grid to Board.

--- board.rb
+++ board.rb
@@ -7,7 +7,35 @@ class Board
   end
 
   def populate_grid
+    [0,2,6].each do |x_coord|
+      [1,3,5,7].each do |y_coord|
+        self[[x_coord, y_coord]] = Piece.new(self, [x_coord, y_coord])
+      end
+    end
 
+    [1,5,7].each do |x_coord|
+      [0,2,4,6].each do |y_coord|
+        self[[x_coord, y_coord]] = Piece.new(self, [x_coord, y_coord])
+      end
+    end
+    nil
+  end
+
+  def [](pos)
+    x, y = pos
+    grid[x][y]
+  end
+
+  def []=(pos, value)
+    x, y = pos
+    grid[x][y] = value
+  end
+
+  def move(start_pos, end_pos)
+    piece = self[start_pos]
+    self[end_pos] = piece
+    self[start_pos] = nil
+    piece.pos = end_pos
   end
 
 end
--- pieces.rb
+++ pieces.rb
@@ -2,29 +2,42 @@ require_relative 'board'
 
 class Piece
 
-  attr_reader :color, :board, :pos
+  attr_reader :board
+  attr_accessor :pos, :color
 
-  def initialize(color, board, pos)
-    @color = color
+  def initialize(board, pos)
+    @color = pos[0].between?(0, 3) ? :white : :black
     @board = board
     @pos = pos
     @king = false
   end
+
+  # def color=(color)
+  #   pos[0].between?(0,3) ? :white : :black
+  # end
 
   def [](pos)
     x, y = pos
     board.grid[x][y]
   end
 
+  def []=(pos, value)
+    x, y = pos
+    board.grid[x][y] = value
+  end
+
   def perform_slide(end_pos)
     x, y = pos
     [[forward_direction, 1], [forward_direction, -1]].each do |delta|
       check_pos = [delta[0] + x, delta[1] + y]
-      return true if valid?(check_pos) &&
-                     check_pos == end_pos &&
-                     self[end_pos].nil?
+      if valid?(check_pos) && check_pos == end_pos && self[end_pos].nil?
+        # move piece
+        # maybe_promote
+      else
+        # raise error: invalid move
+      end
     end
-    false
+
   end
 
   def perform_jump(end_pos)
@@ -38,6 +51,8 @@ class Piece
   def valid?(check_pos)
     check_pos.all? { |el| el.between?(0, 7)}
   end
+
+
 
 
 end
